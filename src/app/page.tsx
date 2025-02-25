35a0280b58762703309634658b0c8e4a1e1c519a
hartod14 <haritody@gmail.com>
2025-02-25 16:21:30 +0700

    first init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,137 @@
+"use client"
+
+import { log } from "console";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const initialDomino: Array<Array<number>> = [
+    [6, 1],
+    [4, 3],
+    [5, 1],
+    [3, 4],
+    [1, 1],
+    [3, 4],
+    [1, 2],
+  ]
+  const [domino, setDomino] = useState<Array<Array<number>>>(initialDomino);
+  const [dobleNumber, setDoubleNumber] = useState<number>(0);
+  const inputRef = useRef<HTMLInputElement>(null)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const countDoubleNumber = () => {
+    setDoubleNumber(domino.reduce((acc, numbers) => {
+      return numbers[0] == numbers[1] ? acc + 1 : acc
+    }, 0))
+  }
+
+  useEffect(() => {
+    countDoubleNumber()
+  }, [])
+
+  const sortAsc = () => {
+    console.log('sort asc');
+    setDomino([...domino].sort((a, b) => {
+      if (a[0] + a[1] == b[0] + b[1]) {
+        return a[0] - b[0];
+      }
+      return (a[0] + a[1]) - (b[0] + b[1])
+    }))
+  }
+
+  const sortDesc = () => {
+    console.log('sort desc');
+    setDomino([...domino].sort((a, b) => {
+      if (a[0] + a[1] == b[0] + b[1]) {
+        return b[0] - a[0];
+      }
+      return (b[0] + b[1]) - (a[0] + a[1])
+    }))
+  }
+
+  const flip = () => {
+    console.log('flip');
+
+    setDomino([...domino].map((numbers) => {
+      return numbers.toReversed()
+    }))
+  }
+
+
+  const reset = () => {
+    console.log('reset');
+
+    setDomino(initialDomino)
+  }
+
+  const removeTotalNumber = () => {
+    console.log('removetotalNumber');
+
+    const inputValue = inputRef.current?.value;
+    setDomino([...domino].filter(([a, b]) => {
+      return a + b != Number(inputValue)
+    }));
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  return (
+    <div className="p-10">
+      <h1 className="font-bold text-4xl mb-3">Dominoes</h1>
+      <div className="w-1/3 mb-2">
+        <div className="p-4 rounded bg-gray-100 border border-gray-300">
+          <h2 className="font-bold text-lg mb-3">Source</h2>
+          <p>{JSON.stringify(initialDomino, null, 2)}</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="w-1/3 mb-4">
+        <div className="p-4 rounded bg-gray-100 border border-gray-300">
+          <h2 className="font-bold text-lg mb-3">Double Number</h2>
+          {dobleNumber}
+        </div>
+      </div>
+      <div className="flex gap-2 mb-4">
+        {domino.map((numbers, index) => (
+          <div key={index} className="flex flex-col gap-2 p-2 border border-gray-500 rounded">
+            <p>{numbers[0]}</p>
+            <p>-</p>
+            <p>{numbers[1]}</p>
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-2 mb-3">
+        <button onClick={sortAsc} className="bg-blue-500 px-3 py-2 rounded text-white">
+          Sort (ASC)
+        </button>
+        <button onClick={sortDesc} className="bg-blue-500 px-3 py-2 rounded text-white">
+          Sort (DESC)
+        </button>
+        <button onClick={flip} className="bg-blue-500 px-3 py-2 rounded text-white">
+          Flip
+        </button>
+        <button onClick={removeDuplicate} className="bg-blue-500 px-3 py-2 rounded text-white">
+          Remove Dup
+        </button>
+        <button onClick={reset} className="bg-blue-500 px-3 py-2 rounded text-white">
+          Reset
+        </button>
+      </div>
+      <div>
+        <input
+          ref={inputRef}
+          type="text"
+          className="px-3 py-2 border border-gray-100 mb-2 w-1/4"
+          placeholder="Input Number"
+        />
+        <div>
+          <div>
+            <button onClick={removeTotalNumber} className="bg-blue-500 px-3 py-2 rounded text-white w-fit">
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
